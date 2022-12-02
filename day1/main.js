f42a34bf475080284fbf861f55735c2b0f65997e
Philippe Eng <peng@heartflow.com>
2022-12-02 14:44:32 -0600

    day1

---
+++ day1/main.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const data = fs.readFileSync('./input.txt', 'utf-8')
+let acc = 0, max = 0
+for (const cal of data.split('\n')) {
+  if (cal === '') {
+    if (acc > max) max = acc
+    acc = 0
+  }
+  else acc += parseInt(cal)
+}
+
+
+console.log(`Elf with the most calories => ${max}`)
+
