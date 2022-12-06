9b5d5e16ad7591dd17500624786a17d530a24b05
Philippe Eng <peng@heartflow.com>
2022-12-05 20:08:30 -0600

    day 5 part 2

---
+++ day5/main-2.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+
+const rows = fs.readFileSync('./input.txt', 'utf-8').split('\n')
+
+let stacks = [], parseMove = false, i = 0
+while (i < rows.length) {
+  let j = 1, k = 0, row = rows[i]
+
+  // Parse stacks
+  while (j < row.length && !parseMove) {
+    if (/[A-Z]/.test(row[j])) {
+      if (!stacks[k]) {
+        stacks[k] = []
+      }
+      stacks[k].unshift(row[j])
+    } else if (row[j] === '1') {
+      parseMove = true
+      i += 2
+      row = rows[i]
+      break
+
+    }
+    j += 4
+    ++k
+  }
+
+  // Parse move and execute them
+  if (parseMove) {
+    if (row === '') break
+    const rowSplit = row.split(' ')
+    let count = parseInt(rowSplit[1])
+    const from = parseInt(rowSplit[3])
+    const to = parseInt(rowSplit[5])
+
+    const arr = []
+    while (count > 0) {
+      if (stacks[from - 1]) {
+        const letter = stacks[from - 1].pop()
+        arr.unshift(letter)
+      }
+      count--
+    }
+    stacks[to - 1] = stacks[to - 1].concat(arr)
+  }
+
+  ++i
+}
+
+const response = stacks.map(s => {
+  return s[s.length - 1]
+}).join('')
+
+console.log('Response =>', response)
+
+
+
